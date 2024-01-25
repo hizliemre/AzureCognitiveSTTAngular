18bb7d3b3f92b9afca5eef1ce04d310f812fe5ab
Emre Hızlı <hizliemre@gmail.com>
2024-01-26 00:50:41 +0300

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,80 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AzureCognitiveSTTAngular';
+  #http = inject(HttpClient);
+  #destroyRef = inject(DestroyRef);
+
+  mediaRecorder?: MediaRecorder;
+  audioChunks: Blob[] = [];
+  downloadLink = signal<string>('');
+  convertedMessage = signal<string>('');
+  status = signal<string>('STOPPED');
+
+  constructor() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(stream => {
+        this.mediaRecorder = new MediaRecorder(stream);
+        this.init();
+      })
+      .catch(err => {
+        console.error("Error accessing the microphone: ", err);
+      });
+  }
+
+  init(): void {
+    if (this.mediaRecorder) {
+      console.log('initialized');
+      fromEvent<BlobEvent>(this.mediaRecorder, 'dataavailable')
+        .pipe(takeUntilDestroyed(this.#destroyRef))
+        .subscribe((event: BlobEvent) => {
+          this.audioChunks.push(event.data);
+        });
+      fromEvent(this.mediaRecorder, 'stop')
+        .pipe(takeUntilDestroyed(this.#destroyRef))
+        .subscribe(() => {
+          console.log('Stopped recording');
+          const audioBlob = new Blob(this.audioChunks, { type: 'audio/webm;codecs=opus' });
+          const audioUrl = URL.createObjectURL(audioBlob);
+          this.downloadLink.set(audioUrl);
+          this.uploadAudio();
+        });
+    }
+  }
+
+  startRecording() {
+    if (this.mediaRecorder) {
+      this.mediaRecorder.start();
+      this.status.set('RECORDING');
+    }
+  }
+
+  stopRecording() {
+    if (this.mediaRecorder) {
+      this.mediaRecorder.stop();
+      this.audioChunks = [];
+      this.status.set('STOPPED');
+    }
+  }
+
+  uploadAudio() {
+    if (!this.downloadLink) {
+      console.error("No audio data available to upload.");
+      return;
+    }
+    const audioBlob = new Blob(this.audioChunks, { type: 'audio/webm;codecs=opus' });
+    const formData = new FormData();
+    formData.append('audio', audioBlob, 'recorded-audio.webm');
+    this.#http.post<string>('http://localhost:5076/upload?lang=en-US', formData)
+      .subscribe({
+        next: (res) => {
+          console.log('Upload successful', res);
+          this.convertedMessage.set(res);
+        },
+        error: (err) => {
+          console.error('Upload error:', err);
+          this.convertedMessage.set('Upload error: ' + err);
+        },
+        complete: () => console.info('Request completed')
+      });
+  }
 }
